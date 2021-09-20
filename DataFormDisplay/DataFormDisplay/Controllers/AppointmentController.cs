368b757ccc62d95bba4e9306327c4a777ec63f54
Hieu Nguyen <73616304+hieunguyen0297@users.noreply.github.com>
2021-09-20 14:48:53 -0400

    complete creating and display

--- DataFormDisplay/DataFormDisplay/Controllers/AppointmentController.cs
+++ DataFormDisplay/DataFormDisplay/Controllers/AppointmentController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataFormDisplay.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,14 +9,24 @@ namespace DataFormDisplay.Controllers
 {
     public class AppointmentController : Controller
     {
+        //this will store all the appointment created and display in index
+        static List<Appointment> appoinments = new List<Appointment>();
         public IActionResult Index()
         {
-            return View();
+            //view a list of appointments
+            //return appointment and view as a list
+            return View(appoinments);
         }
 
         public IActionResult Create()
         {
             return View();
+        }
+
+        public IActionResult Details(Appointment appt)
+        {
+            appoinments.Add(appt);
+            return View("Details",appt);
         }
 
     }

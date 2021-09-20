fb12109f48fe4887439fad53777377a5a4961e0d
Hieu Nguyen <73616304+hieunguyen0297@users.noreply.github.com>
2021-09-20 14:28:15 -0400

    data validation

--- DataFormDisplay/DataFormDisplay/Models/Appointment.cs
+++ DataFormDisplay/DataFormDisplay/Models/Appointment.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +9,24 @@ namespace DataFormDisplay.Models
 {
     public class Appointment
     {
+        [Required]
+        [StringLength(20, MinimumLength = 4)]
+        [DisplayName("Patient's Full Name")]
         public string PatientName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayName("Appointment Request Date")]
         public DateTime AppointmentDate { get; set; }
-        public decimal PatientNetwork { get; set; }
+
+        [DisplayName("Patient's Networth")]
+        [DataType(DataType.Currency)]
+        public decimal PatientNetworth { get; set; }
+
+        [DisplayName("Doctor's Last Name")]
         public string DoctorName { get; set; }
+
+        [Range(1,10)]
+        [DisplayName("Patient's Perceived Level of Pain (1 low to 10 high)")]
         public int PainLevel { get; set; }
     }
 }

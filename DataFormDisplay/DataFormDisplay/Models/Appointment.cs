ab00c0c81dbf91bd8fdc2178aebbb3383769d9e6
Hieu Nguyen <73616304+hieunguyen0297@users.noreply.github.com>
2021-09-20 14:09:48 -0400

    init

---
+++ DataFormDisplay/DataFormDisplay/Models/Appointment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataFormDisplay.Models
+{
+    public class Appointment
+    {
+        public string PatientName { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public decimal PatientNetwork { get; set; }
+        public string DoctorName { get; set; }
+        public int PainLevel { get; set; }
+    }
+}
